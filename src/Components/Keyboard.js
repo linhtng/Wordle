5d1780859ee83cc6e3a84de8b7ba437153d330e6
linhtng <linh.nguyen361991@gmail.com>
2023-08-03 12:14:37 +0300

    keypress

--- src/Components/Keyboard.js
+++ src/Components/Keyboard.js
@@ -1,12 +1,32 @@
 import "../Styles/Keyboard.css"
 import { KeyboardButton } from "./KeyboardButton";
+import { useEffect, useState } from "react";
 
-export const Keyboard = ({fc}) => {
-
+export const Keyboard = ({fc, del}) => {
 	const keys1 = ['q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p'];
 	const keys2 = ['a','s','d','f','g','h','i','j','k','l'];
 	const keys3 = ['z','x','c','v','b','n','m', "DEL"];
 
+	const handleKeyUp = (event) => {
+		const pressedKey = event.key.toLowerCase();
+		if (/^[a-z]$/.test(pressedKey)) {
+			fc(pressedKey);
+		}
+		else if (event.key === "Delete" || event.key === "Backspace") {
+			fc("DEL");
+		}
+	}
+
+	useEffect(() => {
+		// Add event listener for keypress events
+		window.addEventListener("keyup", handleKeyUp);
+	
+		// Clean up the event listener when the component unmounts
+		return () => {
+		  window.removeEventListener("keyup", handleKeyUp);
+		};
+	}, []);
+	
 	return (
 		<>
 			<div className="keyboard-line">
