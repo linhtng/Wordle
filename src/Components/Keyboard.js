992aa7452f8a42d389bba51a8a86851b806573f2
linhtng <linh.nguyen361991@gmail.com>
2023-10-26 18:40:37 +0300

    remove an extra I in keyboard

--- src/Components/Keyboard.js
+++ src/Components/Keyboard.js
@@ -3,10 +3,10 @@ import { KeyboardButton } from "./KeyboardButton";
 import { useEffect, useState } from "react";
 import useEventListener from "@use-it/event-listener";
 
-export const Keyboard = ({fc, del}) => {
+export const Keyboard = ({ fc, del }) => {
 	const keys1 = ['q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p'];
-	const keys2 = ['a','s','d','f','g','h','i','j','k','l'];
-	const keys3 = ['z','x','c','v','b','n','m', "DEL"];
+	const keys2 = ['a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l'];
+	const keys3 = ['z', 'x', 'c', 'v', 'b', 'n', 'm', "DEL"];
 
 	const handleKeyUp = (event) => {
 		const pressedKey = event.key.toLowerCase();
@@ -23,13 +23,13 @@ export const Keyboard = ({fc, del}) => {
 	return (
 		<>
 			<div className="keyboard-line">
-			{ keys1.map((key, i) => <KeyboardButton fc={fc} key={i} text={key} id={i}/>) }
+				{keys1.map((key, i) => <KeyboardButton fc={fc} key={i} text={key} id={i} />)}
 			</div>
 			<div className="keyboard-line">
-			{ keys2.map((key, i) => <KeyboardButton fc={fc} key={i} text={key} id={i}/>) }
+				{keys2.map((key, i) => <KeyboardButton fc={fc} key={i} text={key} id={i} />)}
 			</div>
 			<div className="keyboard-line">
-			{ keys3.map((key, i) => <KeyboardButton fc={fc} key={i} text={key} id={i}/>) }
+				{keys3.map((key, i) => <KeyboardButton fc={fc} key={i} text={key} id={i} />)}
 			</div>
 		</>
 	)
